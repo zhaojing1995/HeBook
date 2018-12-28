ab027661d7653fc9a53827588645c5f460831eff
zhaojing <1326549593@qq.com>
2018-12-28 08:34:39 +0800

    test upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@AUTHOR:Joselyn
+@CONTACT:zhaojing17@foxmail.com
+@SOFTWERE:PyCharm
+@FILE:main.py
+@TIME:2018/12/28 08:33
+@DES:
+'''
+
+if __name__ == "__main__":
+    print "hello zhaojing"
