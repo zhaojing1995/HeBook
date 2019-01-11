141715f6647857d10ce36685862dc3ba014b613f
zhaojing <1326549593@qq.com>
2019-01-11 21:43:33 +0800

    add link to page work_detail

--- web/js/home.js
+++ web/js/home.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
+   $(".people_list").click(function () {
+         window.open("People_list.html",'mainFrame');
+    });
+    $(".work_detail").click(function () {
+         window.open("Work_Detail.html",'mainFrame');
+    });
+
 
 });
