5b1778b52477f0d58960914c7b4513b1c5f9017a
zhaojing <1326549593@qq.com>
2019-01-13 14:57:06 +0800

    common

---
+++ backend/testfile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@AUTHOR:Joselyn
+@CONTACT:zhaojing17@foxmail.com
+@SOFTWERE:PyCharm
+@FILE:testfile.py
+@TIME:2019/1/12 10:44
+@DES:
+'''
+
+
+import MySQLdb
