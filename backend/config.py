ca7ab1dcf825c1b01e057b90e526c26e1a45d6c5
Lony2612 <1060653477@qq.com>
2019-06-01 20:22:26 +0800

    new_config

--- backend/config.py
+++ backend/config.py
@@ -15,3 +15,23 @@ config = {
     "server_ip":"192.168.0.77",
     "database":"hebook"
 }
+
+# 表名称与表项ID间的映射，供修改数据时查找表使用
+table_ID = {
+    "a_contactor_flag":"ContactorID",
+    "a_contactor_like":"ContactorID",
+    "a_contactor_hate":"ContactorID",
+    "t_contactor":"ContactorID",
+    "t_csocialaccount":"SPcountID",
+    "t_edubackdsc":"EduBackDscID",
+    "t_educationbackground":"EduBackID",
+    "t_flag":"FlagID",
+    "t_hate":"HateID",
+    "t_interaction":"InteractionID",
+    "t_job":"JobID",
+    "t_jobdsc":"JobDscID",
+    "t_like":"LikeID",
+    "t_remark":"RemarkID",
+    "t_uscocialaccount":"SPcountID",
+    "t_user":"UserID"
+}
