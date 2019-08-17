09bd81f46075ebf7359ddc59979e75efb6f49029
Lony2612 <1060653477@qq.com>
2019-08-17 20:52:49 +0800

    add authorization

--- backend/CLASS/Authorization.py
+++ backend/CLASS/Authorization.py
@@ -9,3 +9,18 @@
 @TIME:2019/8/17 19:48
 @DES:
 '''
+import database as db
+
+class Authorization():
+    def __init__(self,userid,appname,appaccount):
+        self.AuthorizationID = db.get_current_id('Authorization')
+        self.UserID = userid
+        self.AppName = appname
+        self.AppAccount = appaccount
+        if not db.INSERT('authorization',['authorization_id','user_id','app_name','app_account'],\
+            [self.AuthorizationID,self.UserID,self.AppName,self.AppAccount]):
+            print "Insert error！"
+        
+    def set_AuthorizationCode(self,AuthorizationCode):
+        self.AuthorizationCode = AuthorizationCode
+        db.MODIFIED('authorization',self.AuthorizationID,['Authorization_Code'],[AuthorizationCode])
